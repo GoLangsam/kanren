20b0c1d2ff94c7bfebb4c0cf82d85db47213f5fe
GoLangsam <GoLangsam.APA@a-bc.net>
2020-04-12 21:56:03 +0630

    remove run.go & take.go - no need when using channels

--- internal/µ/run.go
+++
@@ -1,9 +0,0 @@
-package µ
-
-// RunGoal calls a goal with an emptystate and n possible resulting states.
-//
-// Note: If n < 0 (e.g. = -1) then all possible states are returned.
-func RunGoal(n int, g Goal) []S {
-	ss := g(EmptyState())
-	return ss.Take(n)
-}
