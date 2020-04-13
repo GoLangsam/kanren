6ef7a4124493591d93ad8fa0ac8dd2c413229ede
GoLangsam <GoLangsam.APA@a-bc.net>
2020-04-13 13:08:11 +0630

    bind.string.go() prints substitutions now sorted

--- internal/µ/bind/string.go
+++ internal/µ/bind/string.go
@@ -1,11 +1,14 @@
 package bind
 
+import "sort"
+
 func (bind *Ings) String() string {
 	ss := make([]X, 0, len(bind.bound))
 
 	for k, x := range bind.bound {
 		ss = append(ss, cons(k, x))
 	}
+	sort.Slice(ss, func(i, j int) bool { return (ss[i].Car().String() < ss[j].Car().String()) })
 
 	return newList(ss...).String()
 }
